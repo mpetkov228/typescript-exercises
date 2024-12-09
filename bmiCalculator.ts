d7e4af7cead7659ca80cf2f5bb5e3a41a2d93fe3
MihailPetkov96 <mpetkov228@gmail.com>
2024-12-10 00:05:48 +0200

    Complete exercise 9.1 Body mass index

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,5 +1,18 @@
 const calculateBmi = (height: number, weight: number): string => {
-  return '';
+  const bmi = weight / height / height * 10000;
+  if (bmi > 40) {
+    return 'Morbidly Obese range';
+  } else if (bmi > 35) {
+    return 'Severely Obese range';
+  } else if (bmi > 30) {
+    return 'Obese range';
+  } else if (bmi > 25) {
+    return 'Overweight range';
+  } else if (bmi > 18.5) {
+    return 'Normal range';
+  } else {
+    return 'Underweight range';
+  }
 };
 
 console.log(calculateBmi(180, 74));
