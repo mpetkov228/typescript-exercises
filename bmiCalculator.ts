79e8b9c9573f1630c330475d0317d22a77898898
MihailPetkov96 <mpetkov228@gmail.com>
2024-12-10 15:13:25 +0200

    Refactor bmi calc to use command line args

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,18 +1,21 @@
 const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / height / height * 10000;
   if (bmi > 40) {
-    return 'Morbidly Obese range';
+    return 'Morbidly Obese';
   } else if (bmi > 35) {
-    return 'Severely Obese range';
+    return 'Severely Obese';
   } else if (bmi > 30) {
-    return 'Obese range';
+    return 'Obese';
   } else if (bmi > 25) {
-    return 'Overweight range';
+    return 'Overweight';
   } else if (bmi > 18.5) {
     return 'Normal range';
   } else {
-    return 'Underweight range';
+    return 'Underweight';
   }
 };
 
-console.log(calculateBmi(180, 74));
+const height: number = Number(process.argv[2]);
+const weight: number = Number(process.argv[3]);
+
+console.log(calculateBmi(height, weight));
